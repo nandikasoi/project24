c6d2df141f12221c4a457750e685c13c667847fa
nandikasoi <69952137+nandikasoi@users.noreply.github.com>
2020-09-08 13:01:42 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,11 +3,9 @@ const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
-
-function preload()
-{
-	
-}
+ 
+var paper,dustbin;
+var ground;
 
 function setup() {
 	createCanvas(800, 700);
@@ -17,8 +15,10 @@ function setup() {
 	world = engine.world;
 
 	//Create the Bodies Here.
-
-
+	paper = new paper(56,46,55,55);
+	paper.velocityY = 0;
+	dustbin = new Dustbin(100,50,50,100);
+	ground = new Ground(800,20);
 	Engine.run(engine);
   
 }
@@ -27,10 +27,26 @@ function setup() {
 function draw() {
   rectMode(CENTER);
   background(0);
+
+  paper.display();
+  dustbin.display();
+  ground.display();
   
   drawSprites();
  
 }
 
+function keyPressed() {
+	if (keyCode === UP_ARROW) {
 
+       Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x: 85,y: -85});
+
+	}
+}
+
+function keyPressed() {
+	if (keyCode === DOWN_ARROW) {
+       paper.velocityY = -3;
+	}
+}
 
